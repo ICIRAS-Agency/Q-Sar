5db192b26a08907b165d7ebd21a3362611cc9b0c
thomas-lysens <thomas@lyssec.com>
2025-06-17 21:40:17 +0200

    Initiate logger in main so that it can be called over the whole server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+mod utils;
+
 use qsar_lib::qsar;
+use qsar_lib::utils::logger;
 
 #[tokio::main]
 async fn main() {
+    // Init logger
+    let _guards = logger::init_logger();
     let server = qsar::create_server( "127.0.0.1", 3000 ).await;
+    
     match server {
         Ok( addr ) => {
             qsar::listen( addr ).await;
